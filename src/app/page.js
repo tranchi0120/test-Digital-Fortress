122781182d462b4f8d53a655dc974f557e6f2ef2
tranchi0120 <tranchi20012000@gmail.com>
2024-09-19 16:41:35 +0700

    fix setup file

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,11 @@
-import Index from "./pages";
+import LoginPage from "@/app/pages/login";
+import Dashboard from "@/app/pages/Dashboard";
 
 export default function Home() {
     return (
         <div>
-            <Index/>
+            <LoginPage/>
+            <Dashboard/>
         </div>
     );
 }
